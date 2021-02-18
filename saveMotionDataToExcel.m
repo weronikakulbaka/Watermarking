97927078a2b7b4b1ffe42ea1f3522780f33e296d
Paulina Paluch <paulina.paluch1@pollub.edu.pl>
2021-02-18 21:49:59 +0100

    refaktor

--- saveMotionDataToExcel.m
+++ saveMotionDataToExcel.m
@@ -1,20 +1,22 @@
-function[]  =saveMotionDataToExcel(first, second, third,fourth)
-checkforfile=exist(strcat(pwd,'\','MotionAttackResults.xls'),'file');
+function[]  =saveMotionDataToExcel(first, second, third,forth)
+checkforfile=exist(strcat(pwd,'\','BERdataAfterAttacks\','MotionAttackResults.xls'),'file');
 if checkforfile==0 
-    header = {'0.1', '0.5', '1','1.2'};
-    xlswrite('MotionAttackResults',header,'Sheetname','A1');
     N=0;
 else 
- N=size(xlsread('MotionAttackResults','Sheetname'),1);
+ N=size(xlsread('BERdataAfterAttacks\MotionAttackResults','Sheetname'),1);
 end
 AA=strcat('A',num2str(N+2));
 BB=strcat('B',num2str(N+2));
 CC=strcat('C',num2str(N+2));
 DD=strcat('D',num2str(N+2));
 
-xlswrite('MotionAttackResults',first,'Sheetname',AA);
-xlswrite('MotionAttackResults',second,'Sheetname',BB);
-xlswrite('MotionAttackResults',third,'Sheetname',CC);
-xlswrite('MotionAttackResults',fourth,'Sheetname',DD);
+baseFileName = 'MotionAttackResults.xlsx';
+fullFileName = fullfile(strcat(pwd,'\BERdataAfterAttacks'), baseFileName);
+
+xlswrite(fullFileName,first,'Sheetname',AA);
+xlswrite(fullFileName,second,'Sheetname',BB);
+xlswrite(fullFileName,third,'Sheetname',CC);
+xlswrite(fullFileName,forth,'Sheetname',DD);
+
 
 end
--- saveSharpenDataToExcel.m
+++ saveSharpenDataToExcel.m
@@ -1,18 +1,17 @@
-function[]  =saveRotationDataToExcel(first, second, third,forth,fifth,sixth)
-baseFileName = 'Rotation.xlsx';
+function[]  =saveSharpenDataToExcel(first, second, third,forth)
+baseFileName = 'SharpeningAttackResults.xlsx';
 fullFileName = fullfile(strcat(pwd,'\BERdataAfterAttacks'), baseFileName);
-checkforfile=exist(strcat(pwd,'\BERdataAfterAttacks','\','Rotation.xls'),'file');
+checkforfile=exist(strcat(pwd,'\','BERdataAfterAttacks\',baseFileName),'file');
+
 if checkforfile==0 
     N=0;
 else 
- N=size(xlsread('BERdataAfterAttacks\Rotation','Sheetname'),1);
+ N=size(xlsread('BERdataAfterAttacks\SharpeningAttackResults','Sheetname'),1);
 end
 AA=strcat('A',num2str(N+2));
 BB=strcat('B',num2str(N+2));
 CC=strcat('C',num2str(N+2));
 DD=strcat('D',num2str(N+2));
-EE=strcat('E',num2str(N+2));
-FF=strcat('F',num2str(N+2));
 
 
 
@@ -20,8 +19,6 @@ xlswrite(fullFileName,first,'Sheetname',AA);
 xlswrite(fullFileName,second,'Sheetname',BB);
 xlswrite(fullFileName,third,'Sheetname',CC);
 xlswrite(fullFileName,forth,'Sheetname',DD);
-xlswrite(fullFileName,fifth,'Sheetname',EE);
-xlswrite(fullFileName,sixth,'Sheetname',FF);
 
 
 end
